a641b6fc06c8c795611fd9e28da7b5e9475afb55
Dhanunjaya Poolasetty <P7113587@ness.com>
2025-09-10 21:45:40 +0530

    v4

--- dist/my-angular-app/server/angular-app-manifest.mjs
+++ dist/my-angular-app/server/angular-app-manifest.mjs
@@ -30,11 +30,11 @@ export default {
   assets: {
     'index.csr.html': {size: 23481, hash: '28a67ff5608ff9a0911cad5ce521107944682faa999596223f8b3ee0858f9fa5', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
     'index.server.html': {size: 17100, hash: '040e0bd03c656df7ec1638a41385397d9c63a592b2f34e77ca0dbf2c0e35e887', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'contact/index.html': {size: 30930, hash: '6ccfd38ac532c43aecf0c03a7f637dffc4d39cb4ec38a477089e2401b7628ae9', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
-    'edit-product/index.html': {size: 31318, hash: '2d08829b0acd14cb1f9835f218f98ce14d719bc8f331cc04edc37747ad428d8c', text: () => import('./assets-chunks/edit-product_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 33285, hash: 'e39bb9d63f516c84284b0a27557067a0e3caab10f4d785eddd62b68633a8184a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'view-product/index.html': {size: 31178, hash: '617feca4a669acf6cad9eea1a730d2bf60e7e9588510b8d2fb04807a3bddfd36', text: () => import('./assets-chunks/view-product_index_html.mjs').then(m => m.default)},
-    'products/index.html': {size: 33142, hash: '1a3e8502fb4a729dce3d9227dd784e1f03f392991d37566932ab50a34fb17cef', text: () => import('./assets-chunks/products_index_html.mjs').then(m => m.default)},
+    'contact/index.html': {size: 30930, hash: 'eaefc40853503bcd7e15c9979c990bfb9a37bf9dae8436e5f289650572a5f353', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'edit-product/index.html': {size: 31318, hash: 'ff31a89d5244c9022785e5489536c83f9406be063903bb0ae639ec3af0fd6dff', text: () => import('./assets-chunks/edit-product_index_html.mjs').then(m => m.default)},
+    'view-product/index.html': {size: 31178, hash: 'be3f837b581abcf3d1587dab863a8cc76489905e491b55d47c25e883157515a0', text: () => import('./assets-chunks/view-product_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 33285, hash: '5617a160a0a42ade5808d6c929785f1ba80241f74209f9f84aebdaeb4c5a0cca', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'products/index.html': {size: 33142, hash: 'a8330ef9b5c55ab5b027bab5604f7eaa0eeece26313262d222c21f85bd4a136b', text: () => import('./assets-chunks/products_index_html.mjs').then(m => m.default)},
     'styles-Z5UYKNDX.css': {size: 6934, hash: 'VxIyzD7LHG8', text: () => import('./assets-chunks/styles-Z5UYKNDX_css.mjs').then(m => m.default)}
   },
 };
